0a2cefe55f2dbedb4e8e9e5fc6449673eb7c18de
Magomed Magomadov <fayamiyagi1@gmail.com>
2022-04-17 01:10:58 +0300

    react-todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  function important(i) {
+    const importantTodos = todos.map((item, index) => {
+      if (i === index) {
+        return {
+          ...item,
+          freezing: !item.freezing,
+        };
+      }
+      return item;
+    });
+    setTodos(importantTodos);
+  }
+
+  function deleted(indexTodoList) {
+    const deleteTodos = todos.filter((todo, index) => {
+      if (index === indexTodoList) {
+        return false;
+      }
+      {
+        return true;
+      }
+    });
+    setTodos(deleteTodos);
+  }
+  const addTodos = () => {
+    if (text == "") {
+    } else {
+      setTodos([
+        ...todos,
+        {
+          text: text,
+          freezing: false,
+        },
+      ]);
+    }
+    setText("");
+  };
+  const newsTodos = todos.map((todo, index) => {
+    let todoClass;
+    let sectionSex;
+    let todoDel;
+    if (todo.freezing === false) {
+      sectionSex = "section";
+      todoDel = "✖";
+    } else {
+      sectionSex = "sectionSex";
+      todoClass = "hiding";
+    }
+
+    return (
+      <div className={sectionSex}>
+        <div className="mark true">
+          <button onClick={() => important(index)} className="checkMark">
+            ★
+          </button>
+        </div>
+        <div className="text">
+          <span>{todo.text}</span>
+        </div>
+        <div className="deleteTodo">
+          <span onClick={() => deleted(index)} className={todoClass}>
+            {todoDel}
+          </span>
+        </div>
+      </div>
+    );
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todoListText">
+        <span>Список дел</span>
+      </div>
+      <div className="header">
+        <input
+          className="val"
+          placeholder={"Введите текст ..."}
+          value={text}
+          type="text"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={addTodos}>Добавить</button>
+      </div>
+      {newsTodos}
     </div>
   );
+
 }
 
 export default App;
+
+
